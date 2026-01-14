0e94cf1cd7f4ed8aff5362452b6029cc90c895e6
Cheng-Hsien1208 <chenghsien1208@gmail.com>
2026-01-14 09:22:35 +0800

    apply gitnore rule

--- shell_script/hw/docker_exec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xhost +
-docker container start legged_hil_hw
-docker exec -it legged_hil_hw bash
--- shell_script/sim/docker_exec.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xhost +
-docker container start legged_hil_sim
-docker exec -it legged_hil_sim bash
